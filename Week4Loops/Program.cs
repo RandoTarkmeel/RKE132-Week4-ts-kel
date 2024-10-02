1040c90de9395da0c6499a6ee13b4d5427601353
unknown <Rando.Tarkmeel@tktk.ee git config --global user.name>
2024-10-02 19:08:17 +0300

    add week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i = 0;
+for (i = 0; i < 5; i++)
+{
+    Console.WriteLine("we don´t need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
